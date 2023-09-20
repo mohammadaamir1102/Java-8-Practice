cb9957681466fb1d21b379ee3976b2b5ab881543
mohammadaamir1102 <mohammadaamir1102@gmail.com>
2023-09-20 18:39:14 +0530

    conflict solve

--- src/com/session/Practice.java
+++ src/com/session/Practice.java
@@ -1,29 +1,16 @@
 package com.session;
 
-<<<<<<< HEAD
 
 import java.util.Objects;
-
-public class Practice {
-
-    public static void main(String[] args) {
-        try {
-            method1();
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.out.println("Catch block");
-=======
 public class Practice {
     public static void main(String[] args) throws Exception {
         String name="L26";
         if(name.contains(name)){
             System.out.println("exist");
->>>>>>> 85995503eb7a621d29e928562f060569bc7cdaa1
         }
 
     }
 
-<<<<<<< HEAD
     private static void method1() {
         method2();
 
@@ -38,6 +25,4 @@ public class Practice {
         System.out.println(a);
     }
 
-=======
->>>>>>> 85995503eb7a621d29e928562f060569bc7cdaa1
 }
